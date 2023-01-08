25bd388c70db604b36e2571b690fd589bda50fb4
john-farina <clichecontent@gmail.com>
2023-01-08 18:23:13 -0500

    Cleaned up code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { zoltarFortunes } from './scripts/zoltarFortunes';
-import { Howl, Howler } from 'howler';
-import CircleType from 'circletype';
+import { zoltarFortunes } from "./scripts/zoltarFortunes";
+import { Howl } from "howler";
+import CircleType from "circletype";
 let fortuneTicket;
 let fortuneText;
 let luckyNumbers;
@@ -8,298 +8,282 @@ let hasHadFirstFortune = false;
 let isTalking = false;
 let cardIsOpen = false;
 
-//create random fortune cards
-//random choice for the saying on card (15 choices so far)
-//6 random lucky numbers at the bottom (without repeating a number)
-// EX: Your Lucky Numbers: 08, 16, 24, 22, 10, 09
-//simple enough
+const body = document.querySelector("body");
 
-const body = document.querySelector('body');
-
-//CIRCLE TEXT CREATION
-
-const circleType = new CircleType(document.getElementById('zoltarLogo'));
+const circleType = new CircleType(document.getElementById("zoltarLogo"));
 circleType.radius(500).dir(1);
 
 function randomNumGen(number) {
-    let randomNum = Math.floor(Math.random() * number);
-    return randomNum;
+  let randomNum = Math.floor(Math.random() * number);
+
+  return randomNum;
 }
 
 function createLuckyNumString(array) {
-    let newString = '';
-    for (let i = 0; i < array.length; i++) {
-        let newNum = array[i];
-        let newNumString = newNum.toString();
-        if (i === 5) {
-            newString = newString + `${newNumString}. `;
-        } else {
-            newString = newString + `${newNumString}, `;
-        }
+  let newString = "";
+
+  for (let i = 0; i < array.length; i++) {
+    let newNum = array[i];
+    let newNumString = newNum.toString();
+
+    if (i === 5) {
+      newString = newString + `${newNumString}. `;
+    } else {
+      newString = newString + `${newNumString}, `;
     }
-    return newString;
+  }
+
+  return newString;
 }
+
+function stylizeNumber(num) {
+  if (num <= 9) {
+    // no zeros
+    if (num === 0) {
+      return "08";
+    }
+
+    return "0" + num;
+  }
+
+  return num.toString();
+}
+
 function randomNumOutOf99() {
-    let randomNum = randomNumGen(100);
-    if (randomNum === 0) {
-        randomNum = '08';
-    }
-    if (randomNum === 1) {
-        randomNum = '01';
-    }
-    if (randomNum === 2) {
-        randomNum = '02';
-    }
-    if (randomNum === 3) {
-        randomNum = '03';
-    }
-    if (randomNum === 4) {
-        randomNum = '04';
-    }
-    if (randomNum === 5) {
-        randomNum = '05';
-    }
-    if (randomNum === 6) {
-        randomNum = '06';
-    }
-    if (randomNum === 7) {
-        randomNum = '07';
-    }
-    if (randomNum === 8) {
-        randomNum = '08';
-    }
-    if (randomNum === 9) {
-        randomNum = '09';
-    }
-    randomNum = randomNum.toString();
-    return randomNum;
+  let randomNum = randomNumGen(100);
+
+  randomNum = stylizeNumber(randomNum);
+
+  return randomNum;
 }
+
 function generateLuckyNumbers() {
-    let newArray = [];
-    for (let i = 0; i < 6; i++) {
-        newArray[i] = randomNumOutOf99();
-    }
-    let numberString = createLuckyNumString(newArray);
-    let fortuneString = `Your Lucky Numbers: ${numberString}`;
-    return fortuneString;
+  let newArray = [];
+
+  for (let i = 0; i < 6; i++) {
+    newArray[i] = randomNumOutOf99();
+  }
+
+  return `Your Lucky Numbers: ${createLuckyNumString(newArray)}`;
 }
 
 function generateRandomFortune() {
-    let randomNum = randomNumGen(zoltarFortunes.length);
-    let newFortune = zoltarFortunes[randomNum];
-    return newFortune;
+  let randomNum = randomNumGen(zoltarFortunes.length);
+
+  return zoltarFortunes[randomNum];
 }
 
 function changeFortuneText(string) {
-    let newFortune = (fortuneText.innerHTML = string);
-    return newFortune;
+  let newFortune = (fortuneText.innerHTML = string);
+
+  return newFortune;
 }
+
 function changeLuckyNumberText() {
-    let luckyNumber = generateLuckyNumbers();
-    luckyNumbers.innerHTML = luckyNumber;
+  let luckyNumber = generateLuckyNumbers();
+
+  luckyNumbers.innerHTML = luckyNumber;
 }
+
 function createFortuneTicket() {
-    fortuneTicket = document.createElement('div');
-    fortuneTicket.classList.add('fortuneTicket');
+  const closeButton = document.createElement("button");
+  const fortuneHeader = document.createElement("h2");
+  const lineBreak = document.createElement("div");
+  const textContainer = document.createElement("div");
+  const bottomHeader = document.createElement("h2");
+  const lineBreakTwo = document.createElement("div");
 
-    const closeButton = document.createElement('button');
-    closeButton.classList.add('fortuneCloseButton');
-    closeButton.innerHTML = 'X';
-    closeButton.addEventListener('click', function () {
-        fortuneTicket.classList.add('throwOutAnimation');
-        setTimeout(function () {
-            cardIsOpen = false;
-            body.removeChild(fortuneTicket);
-        }, 5000);
-    });
+  fortuneTicket = document.createElement("div");
+  fortuneTicket.classList.add("fortuneTicket");
 
-    const fortuneHeader = document.createElement('h2');
-    fortuneHeader.classList.add('fortuneHeader');
-    fortuneHeader.classList.add('header');
-    fortuneHeader.innerHTML = 'YOUR FORTUNE';
+  closeButton.classList.add("fortuneCloseButton");
+  closeButton.innerHTML = "X";
 
-    const lineBreak = document.createElement('div');
-    lineBreak.classList.add('lineBreak');
-    lineBreak.classList.add('lineOne');
+  closeButton.addEventListener("click", function () {
+    fortuneTicket.classList.add("throwOutAnimation");
+    setTimeout(function () {
+      cardIsOpen = false;
+      body.removeChild(fortuneTicket);
+    }, 5000);
+  });
 
-    const textContainer = document.createElement('div');
-    textContainer.classList.add('text-container');
-    fortuneText = document.createElement('p');
-    changeFortuneText(generateRandomFortune());
-    textContainer.appendChild(fortuneText);
+  fortuneHeader.classList.add("fortuneHeader");
+  fortuneHeader.classList.add("header");
+  fortuneHeader.innerHTML = "YOUR FORTUNE";
 
-    const bottomHeader = document.createElement('h2');
-    bottomHeader.classList.add('fortuneHeader');
-    bottomHeader.classList.add('bottom');
-    bottomHeader.innerHTML = 'PLAY AGAIN!';
+  lineBreak.classList.add("lineBreak");
+  lineBreak.classList.add("lineOne");
 
-    const lineBreakTwo = document.createElement('div');
-    lineBreakTwo.classList.add('lineBreak');
-    lineBreakTwo.classList.add('lineTwo');
+  textContainer.classList.add("text-container");
 
-    luckyNumbers = document.createElement('p');
-    changeLuckyNumberText();
+  fortuneText = document.createElement("p");
+  changeFortuneText(generateRandomFortune());
 
-    fortuneTicket.appendChild(closeButton);
-    fortuneTicket.appendChild(fortuneHeader);
-    fortuneTicket.appendChild(lineBreak);
-    fortuneTicket.appendChild(textContainer);
-    fortuneTicket.appendChild(bottomHeader);
-    fortuneTicket.appendChild(lineBreakTwo);
-    fortuneTicket.appendChild(luckyNumbers);
-    body.appendChild(fortuneTicket);
-    cardIsOpen = true;
+  textContainer.appendChild(fortuneText);
+
+  bottomHeader.classList.add("fortuneHeader");
+  bottomHeader.classList.add("bottom");
+  bottomHeader.innerHTML = "PLAY AGAIN!";
+
+  lineBreakTwo.classList.add("lineBreak");
+  lineBreakTwo.classList.add("lineTwo");
+
+  luckyNumbers = document.createElement("p");
+  changeLuckyNumberText();
+
+  fortuneTicket.appendChild(closeButton);
+  fortuneTicket.appendChild(fortuneHeader);
+  fortuneTicket.appendChild(lineBreak);
+  fortuneTicket.appendChild(textContainer);
+  fortuneTicket.appendChild(bottomHeader);
+  fortuneTicket.appendChild(lineBreakTwo);
+  fortuneTicket.appendChild(luckyNumbers);
+
+  body.appendChild(fortuneTicket);
+
+  cardIsOpen = true;
 }
-// createFortuneTicket();
 
 //SOUND DESIGN
 function randomSoundSrc() {
-    let randomNum = randomNumGen(4);
-    let srcString = '';
-    if (randomNum === 0) {
-        srcString = '/src/audio/zoltar1.mp3';
-        console.log('1');
-    } else if (randomNum === 1) {
-        srcString = '/src/audio/zoltar2.mp3';
-        console.log('2');
-    } else if (randomNum === 2) {
-        srcString = '/src/audio/zoltar3.mp3';
-        console.log('3');
-    } else if (randomNum === 3) {
-        srcString = '/src/audio/zoltar4.mp3';
-        console.log('4');
-    }
+  let randomNum = randomNumGen(4);
+  let srcString = "";
+
+  if (randomNum === 0) {
+    srcString = "/src/audio/zoltar1.mp3";
+    console.log("1");
     return srcString;
+  }
+
+  if (randomNum === 1) {
+    srcString = "/src/audio/zoltar2.mp3";
+    console.log("2");
+    return srcString;
+  }
+
+  if (randomNum === 2) {
+    srcString = "/src/audio/zoltar3.mp3";
+    console.log("3");
+    return srcString;
+  }
+
+  if (randomNum === 3) {
+    srcString = "/src/audio/zoltar4.mp3";
+    console.log("4");
+    return srcString;
+  }
 }
 
 function zoltarSpeech() {
-    const sound = new Howl({
-        src: [randomSoundSrc()],
-        volume: 0.6,
-    });
-    sound.play();
-    sound.on('play', function () {});
-    sound.on('end', function () {
-        isTalking = false;
+  const sound = new Howl({
+    src: [randomSoundSrc()],
+    volume: 0.6,
+  });
 
-        const ticket = new Howl({
-            src: ['/src/audio/ticketSound.mp3'],
-            volume: 0.1,
-        });
-        ticket.play();
-        ticket.on('play', function () {
-            createFortuneTicket();
-        });
+  sound.play();
+
+  sound.on("end", function () {
+    isTalking = false;
+
+    const ticket = new Howl({
+      src: ["/src/audio/ticketSound.mp3"],
+      volume: 0.1,
     });
+
+    ticket.play();
+
+    ticket.on("play", function () {
+      createFortuneTicket();
+    });
+  });
 }
 
-const topLampOne = document.querySelector('.lampOne');
-const topLampTwo = document.querySelector('.lampTwo');
-const lightOne = document.querySelector('.lightOne');
-const lightTwo = document.querySelector('.lightTwo');
-const magicBall = document.querySelector('#magicBall');
+const topLampOne = document.querySelector(".lampOne");
+const topLampTwo = document.querySelector(".lampTwo");
+const lightOne = document.querySelector(".lightOne");
+const lightTwo = document.querySelector(".lightTwo");
+const magicBall = document.querySelector("#magicBall");
 
 function startAnimationWhenTalking() {
-    if (isTalking === true) {
-        divButton.classList.remove('growUp');
-        zoltarHand.classList.add('moveHand');
-        magicBall.classList.add('ballColorChange');
-        topLampOne.classList.add('lampAnimation');
-        topLampTwo.classList.add('lampAnimation');
-        lightOne.classList.add('lightAnimation');
-        lightTwo.classList.add('lightAnimation');
-    } else if (isTalking === false) {
-        divButton.classList.add('growUp');
-        zoltarHand.classList.remove('moveHand');
-        magicBall.classList.remove('ballColorChange');
-        topLampOne.classList.remove('lampAnimation');
-        topLampTwo.classList.remove('lampAnimation');
-        lightOne.classList.remove('lightAnimation');
-        lightTwo.classList.remove('lightAnimation');
-    }
-}
+  if (isTalking === true) {
+    divButton.classList.remove("growUp");
 
-// createFortuneTicket();
+    zoltarHand.classList.add("moveHand");
+
+    magicBall.classList.add("ballColorChange");
+
+    topLampOne.classList.add("lampAnimation");
+    topLampTwo.classList.add("lampAnimation");
+
+    lightOne.classList.add("lightAnimation");
+    lightTwo.classList.add("lightAnimation");
+  } else if (isTalking === false) {
+    divButton.classList.add("growUp");
+
+    zoltarHand.classList.remove("moveHand");
+
+    magicBall.classList.remove("ballColorChange");
+
+    topLampOne.classList.remove("lampAnimation");
+    topLampTwo.classList.remove("lampAnimation");
+
+    lightOne.classList.remove("lightAnimation");
+    lightTwo.classList.remove("lightAnimation");
+  }
+}
 
 function updateAnimations() {
-    startAnimationWhenTalking();
+  startAnimationWhenTalking();
 }
+
 function animationLoop() {
-    updateAnimations();
+  updateAnimations();
 }
+
 function start() {
-    setInterval(animationLoop, 400);
+  setInterval(animationLoop, 400);
 }
+
 start();
 
-const divButton = document.querySelector('#divButton');
-divButton.addEventListener('click', function () {
-    if (hasHadFirstFortune === false && isTalking != true) {
-        const coin = new Howl({
-            src: ['/src/audio/coinSound.mp3'],
-            volume: 0.4,
-        });
-        coin.play();
-        coin.on('play', function () {
-            isTalking = true;
-        });
-        coin.on('end', function () {
-            zoltarSpeech();
-        });
+const divButton = document.querySelector("#divButton");
 
-        // createFortuneTicket();
-        hasHadFirstFortune = true;
-    } else if (
-        hasHadFirstFortune === true &&
-        isTalking != true &&
-        cardIsOpen != true
-    ) {
-        // body.removeChild(fortuneTicket);
-        const coin = new Howl({
-            src: ['/src/audio/coinSound.mp3'],
-            volume: 0.7,
-        });
-        coin.play();
-        coin.on('play', function () {
-            isTalking = true;
-        });
-        coin.on('end', function () {
-            zoltarSpeech();
-        });
-        // createFortuneTicket();
-    }
-    // createFortuneTicket();
+divButton.addEventListener("click", function () {
+  if (hasHadFirstFortune === false && isTalking != true) {
+    const coin = new Howl({
+      src: ["/src/audio/coinSound.mp3"],
+      volume: 0.4,
+    });
+
+    coin.play();
+
+    coin.on("play", function () {
+      isTalking = true;
+    });
+
+    coin.on("end", function () {
+      zoltarSpeech();
+    });
+
+    hasHadFirstFortune = true;
+  } else if (
+    hasHadFirstFortune === true &&
+    isTalking != true &&
+    cardIsOpen != true
+  ) {
+    const coin = new Howl({
+      src: ["/src/audio/coinSound.mp3"],
+      volume: 0.7,
+    });
+
+    coin.play();
+
+    coin.on("play", function () {
+      isTalking = true;
+    });
+
+    coin.on("end", function () {
+      zoltarSpeech();
+    });
+  }
 });
-
-// const testButton = document.querySelector('.testButton');
-// testButton.addEventListener('click', function () {
-//     if (hasHadFirstFortune === false && isTalking != true) {
-//         const coin = new Howl({
-//             src: ['/src/audio/coinSound.mp3'],
-//             volume: 0.4,
-//         });
-//         coin.play();
-//         coin.on('end', function () {
-//             zoltarSpeech();
-//         });
-
-//         // createFortuneTicket();
-//         hasHadFirstFortune = true;
-//     } else if (
-//         hasHadFirstFortune === true &&
-//         isTalking != true &&
-//         cardIsOpen != true
-//     ) {
-//         // body.removeChild(fortuneTicket);
-//         const coin = new Howl({
-//             src: ['/src/audio/coinSound.mp3'],
-//             volume: 0.7,
-//         });
-//         coin.play();
-//         coin.on('end', function () {
-//             zoltarSpeech();
-//         });
-//         // createFortuneTicket();
-//     }
-//     // createFortuneTicket();
-// });
